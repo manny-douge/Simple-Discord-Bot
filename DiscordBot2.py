3bbd5920d131b802c74dc06c9b741e9214a8c6f6
stevesweetney <stevesweetney@gmail.com>
2016-07-28 03:08:34 -0400

    Update DiscordBot2.py

--- DiscordBot2.py
+++ DiscordBot2.py
@@ -6,31 +6,27 @@ import requests
 if not discord.opus.is_loaded():
     discord.opus.load_opus('opus')
 
-leaguedict={
-     }
+leaguedict={}
 
 bot = discord.Client()
-
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-    if "my summoner name is " in message.content.lower():
+    elif "my summoner name is " in message.content.lower():
         name = message.author.name.lower()
         lolname=message.content.lower()[20:]
         leaguedict[name]=lolname
 
-    if "league id" in message.content.lower():
+    elif "league id" in message.content.lower():
         name=message.author.name.lower()
         summoner=leaguedict[name]
         r=requests.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/'+summoner+'?api_key=RGAPI-0E9F7F62-AE11-44E6-8ECE-D76945CB4EB0')
         requestJSON=r.json()
         id=(requestJSON[summoner]['id'])
         await bot.send_message(message.channel, 'Your summoner ID is '+str(id), tts=False)
-
-
-
+        
     if '!casual' in message.content.lower():
         await bot.send_message(message.channel,
                                "http://i1.kym-cdn.com/entries/icons/original/000/013/017/casual.jpg", tts=False)
